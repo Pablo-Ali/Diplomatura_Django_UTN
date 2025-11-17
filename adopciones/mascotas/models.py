2a7d57367a56991e45adfcffd71f03351118c4c6
Pablo-Ali <pablosharifali@gmail.com>
2025-11-17 09:25:29 -0300

    add: orm para las mascotas

--- adopciones/mascotas/models.py
+++ adopciones/mascotas/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils.html import format_html # nos permite usar formato html
 
-# Create your models here.
+class Especie(models.Model):
+    nombre = models.CharField(max_length=10, db_index=True)
+
+    def __str__(self):
+        return '%s' % self.nombre
+
+class Mascota(models.Model):
+    #tupla de opciones para el estado
+    Adoptado = 'Adoptado'
+    En_Adopcion = 'En adopción'
+    ESTADO_MASCOTA = (
+        (Adoptado, 'Adoptado'),
+        (En_Adopcion, 'En adopción'),
+    )
+
+    estado = models.CharField(max_length=10, choices=ESTADO_MASCOTA, default='En adopción')
+    nombre_mascota = models.CharField(max_length=200)
+    fecha_publicacion = models.DateTimeField('Fecha de publicación')
+    imagen = models.ImageField(upload_to="nombre_mascota/%Y/%m/%d", blank=True, null=True)
+    especie = models.ForeignKey(
+        Especie, blank=False, null=True, on_delete=models.CASCADE
+    )
+
+
+    def estado_de_mascota(self):
+        if self.estado == 'Adoptado':
+            return format_html('<span style="color: #f00;">{}</span>', self.estado, )
+        elif self.estado == 'En adopción':
+            return format_html('<span style="color: #f0f;">{}</span>', self.estado, )
+    
+    
+    def __str__(self, ):
+        return self.nombre_mascota
