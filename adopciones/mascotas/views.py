5c9e343a100579f882b6284d20d85796007349c1
Pablo-Ali <pablosharifali@gmail.com>
2025-11-23 14:15:21 -0300

    add: página de mascotas y estilos propios

--- adopciones/mascotas/views.py
+++ adopciones/mascotas/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Mascota
 
-def index(request): #agrego un index básico para que no se rompa la aplicación
-    
-    return render(request, 'mascotas/index.html')
+def index(request):
+    mascotas = Mascota.objects.filter(estado='En adopción')
+    return render(request, "mascotas/index.html", {"mascotas": mascotas})
+
+
+
+
+#def index(request): #agrego un index básico para que no se rompa la aplicación 
+#    return render(request, 'mascotas/index.html')
